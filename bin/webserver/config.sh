405041722c6c9a5594105b788d2fcf4b8dae6b4f
Xibitol <xibitol@pimous.dev>
2026-01-21 13:58:48 +0100

    Added development web server's configuration.

---
+++ bin/webserver/config.sh
@@ -0,0 +1,6 @@
+export DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+
+export DOCKER_IMAGE_REFERENCE=theTypist/front:2.10.0-alpine
+export DOCKER_CONTAINER_NAME=theTypist-front
+
+export SUBPROJECT_DEV_OUTPUT_DIR=$PWD/out/front/dev
--- bin/webserver/start.sh
+++ bin/webserver/start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "Start dev webserver"
+SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+#shellcheck source=../dockerUtils.sh
+source "bin/dockerUtils.sh"
+
+source "$SCRIPT_DIR"/config.sh
+
+# ---
+mode=${1-"debug"}
+resourceDir=${2-$SCRIPT_DIR}
+
+# ---
+stop "$DOCKER_CONTAINER_NAME"
+
+removeImage "$DOCKER_IMAGE_REFERENCE"
+buildImage "$DOCKER_IMAGE_REFERENCE" "$DOCKER_FILE" "$resourceDir"
+
+createContainer \
+	"$DOCKER_CONTAINER_NAME" "$DOCKER_IMAGE_REFERENCE" "$mode" \
+	--mount "type=bind,src=$SUBPROJECT_DEV_OUTPUT_DIR,dst=/usr/share/caddy,readonly"
