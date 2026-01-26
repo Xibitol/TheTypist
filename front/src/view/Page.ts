f69a9ef515e9ef35b3bf0f4798ebcd18288f6fe3
Xibitol <xibitol@pimous.dev>
2026-01-26 21:41:36 +0100

    Added page events triggered on visibility change.

--- front/src/view/Page.ts
+++ front/src/view/Page.ts
@@ -4,14 +4,24 @@ import pageStyle from "@r/style/page.css?string";
 
 export default abstract class Page extends View{
 
+	public static readonly VISIBILITY_EVENT_NAME = "visibility";
+
 	constructor(htmlTemplate: string, cssStyle: string){
 		super(htmlTemplate, cssStyle + pageStyle);
 	}
 
 	// GETTERS
-	public isShown(){ return this.style.display === ""; }
+	public isShown(){
+		return this.style.display === "";
+	}
 
 	// FUNCTIONS
-	public hide(){ this.style.display = "none"; }
-	public show(){ this.style.display = ""; }
+	public hide(){
+		this.style.display = "none";
+		this.dispatchEvent(new Event(Page.VISIBILITY_EVENT_NAME));
+	}
+	public show(){
+		this.style.display = "";
+		this.dispatchEvent(new Event(Page.VISIBILITY_EVENT_NAME));
+	}
 }
